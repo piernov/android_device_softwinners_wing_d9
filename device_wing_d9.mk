61c8d5e5c0a99d0534a9748639199bc8c363c0d5
piernov <piernov@piernov.org>
2014-08-09 21:00:28 +0200

    Move kernel modules to modules/ directory + Fix typo init_parttion.sh -> init_partitions.sh + delete useless files

--- device_wing_d9.mk
+++ device_wing_d9.mk
@@ -19,16 +19,15 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, build/target/product/full.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_wing_d9
 PRODUCT_DEVICE := wing_d9
 
 # Custom stuff
 PRODUCT_COPY_FILES += \
-	device/softwinners/wing_d9/extra/disp.ko:recovery/root/disp.ko \
-	device/softwinners/wing_d9/extra/hdmi.ko:recovery/root/hdmi.ko \
-	device/softwinners/wing_d9/extra/lcd.ko:recovery/root/lcd.ko \
-	device/softwinners/wing_d9/extra/nand.ko:recovery/root/nand.ko \
-	device/softwinners/wing_d9/extra/init_parttion.sh:recovery/root/sbin/init_parttion.sh \
-	device/softwinners/wing_d9/extra/ueventd.sun7i.rc:recovery/root/ueventd.sun7i.rc \
-	device/softwinners/wing_d9/extra/ft5x_ts.ko:recovery/root/ft5x_ts.ko
+	device/softwinners/wing_d9/modules/disp.ko:recovery/root/disp.ko \
+	device/softwinners/wing_d9/modules/hdmi.ko:recovery/root/hdmi.ko \
+	device/softwinners/wing_d9/modules/lcd.ko:recovery/root/lcd.ko \
+	device/softwinners/wing_d9/modules/nand.ko:recovery/root/nand.ko \
+	device/softwinners/wing_d9/modules/ft5x_ts.ko:recovery/root/ft5x_ts.ko \
+	device/softwinners/wing_d9/extra/init_partitions.sh:recovery/root/sbin/init_partitions.sh \
+	device/softwinners/wing_d9/extra/ueventd.sun7i.rc:recovery/root/ueventd.sun7i.rc
